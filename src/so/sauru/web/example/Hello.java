45a304024f7294092d269cd6e16b4a544e578e84
Yonghwan SO <sio4@users.sf.net>
2014-12-22 12:56:43 +0900

    fix router structure

--- src/so/sauru/web/example/Hello.java
+++ src/so/sauru/web/example/Hello.java
@@ -21,7 +21,7 @@ public class Hello extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
+		PrintWriter out = resp.getWriter();
 		out.println("Hello");
 	}
 
