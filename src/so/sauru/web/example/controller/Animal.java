a91b968bc58455967f458215c98d4e0b67bda244
Yonghwan SO <sio4@users.sf.net>
2014-12-31 21:15:41 +0900

    fix example controllers for new recursion test.

--- src/so/sauru/web/example/controller/Animal.java
+++ src/so/sauru/web/example/controller/Animal.java
@@ -1,5 +1,6 @@
 package so.sauru.web.example.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import so.sauru.web.restar.Controller;
@@ -7,10 +8,23 @@ import so.sauru.web.restar.Controller;
 public class Animal extends Controller {
 
 	@Override
-	public Object index(HashMap<String, Object> params) {
+	public HashMap<String, Object> index(HashMap<String, Object> params) {
 		HashMap<String, Object> data = new HashMap<String, Object>();
-		data.put("name", "Tiger");
-		data.put("id", params.get("id"));
+		HashMap<String, Object> elem1 = new HashMap<String, Object>();
+		HashMap<String, Object> elem2 = new HashMap<String, Object>();
+		ArrayList<HashMap<?, ?>> list = new ArrayList<HashMap<?, ?>>();
+		elem1.put("name", "Tiger");
+		elem1.put("id", params.get("argument"));
+		list.add(elem1);
+		if (((String) params.get("argument")).equals("*")) {
+			logger.trace("OK, argument is *. add more...");
+			elem2.put("name", "Lion");
+			elem2.put("id", params.get("argument"));
+			list.add(elem2);
+		} else {
+			logger.trace("OK, just for " + (String) params.get("argument"));
+		}
+		data.put(this.getClass().getSimpleName().toLowerCase(), list);
 		return data;
 	}
 }
--- src/so/sauru/web/example/controller/Leg.java
+++ src/so/sauru/web/example/controller/Leg.java
@@ -8,7 +8,7 @@ import so.sauru.web.restar.Controller;
 public class Leg extends Controller {
 
 	@Override
-	public Object index(HashMap<String, Object> params) {
+	public HashMap<String, Object> index(HashMap<String, Object> params) {
 		HashMap<String, Object> data = new HashMap<String, Object>();
 		HashMap<String, Object> elem1 = new HashMap<String, Object>();
 		HashMap<String, Object> elem2 = new HashMap<String, Object>();
@@ -19,20 +19,23 @@ public class Leg extends Controller {
 		elem1.put("length", "600");
 		elem1.put("finger", "4");
 		list.add(elem1);
-		elem2.put("name", "front-right");
-		elem2.put("length", "600");
-		elem2.put("finger", "4");
-		list.add(elem2);
-		elem3.put("name", "rear-left");
-		elem3.put("length", "700");
-		elem3.put("finger", "4");
-		list.add(elem3);
-		elem4.put("name", "rear-right");
-		elem4.put("length", "700");
-		elem4.put("finger", "4");
-		list.add(elem4);
-
-		data.put("legs", list);
+		if (((String) params.get("argument")).equals("*")) {
+			elem2.put("name", "front-right");
+			elem2.put("length", "600");
+			elem2.put("finger", "4");
+			list.add(elem2);
+			elem3.put("name", "rear-left");
+			elem3.put("length", "700");
+			elem3.put("finger", "4");
+			list.add(elem3);
+			elem4.put("name", "rear-right");
+			elem4.put("length", "700");
+			elem4.put("finger", "4");
+			list.add(elem4);
+		} else {
+			logger.trace("ok, just for " + (String) params.get("argument"));
+		}
+		data.put(this.getClass().getSimpleName().toLowerCase(), list);
 		return data;
 	}
 }
