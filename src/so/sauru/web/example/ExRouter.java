087245dba811306871c3a59f0a18414739f9790f
Yonghwan SO <sio4@users.sf.net>
2015-01-02 05:03:48 +0900

    apply custom init method.

--- src/so/sauru/web/example/ExRouter.java
+++ src/so/sauru/web/example/ExRouter.java
@@ -10,4 +10,8 @@ import so.sauru.web.restar.Router;
 @WebServlet(description = "Path Router", urlPatterns = { "/ExRouter/*" })
 public class ExRouter extends Router {
 	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void customInit() {
+	}
 }
